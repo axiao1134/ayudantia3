038e9ca8ee9639f15c401ee141d5fcb7ffc80b70
Xiao <z.xiao01@ufromail.cl>
2023-09-30 17:56:39 -0300

    version 3.0

--- src/main/java/Biblioteca.java
+++ src/main/java/Biblioteca.java
@@ -1,15 +1,39 @@
 import java.util.List;
 import java.util.ArrayList;
 public class Biblioteca {
-    private String nombre;
+    private String nombreBil;
     private String direccion;
     private List<Libros> listaLibros;
 
     // Constructor
-    public Biblioteca(String nombre, String direccion) {
-        this.nombre = nombre;
+    public Biblioteca(String nombreBil, String direccion) {
+        this.nombreBil = nombreBil;
         this.direccion = direccion;
         this.listaLibros = new ArrayList<>();
     }
+    public String getNombre() {
+        return nombreBil;
+    }
+
+    public void setNombre(String nombrebil) {
+        this.nombreBil = nombrebil;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public List<Libros> getListaLibros() {
+        return listaLibros;
+    }
+
+    public void setListaLibros(List<Libros> listaLibros) {
+        this.listaLibros = listaLibros;
+    }
+
 
 }
