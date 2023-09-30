4d96e01b2dc4f37fff2b3d779debccfd6fc9790c
Xiao <z.xiao01@ufromail.cl>
2023-09-30 17:36:51 -0300

    version 2.0

--- src/main/java/Libros.java
+++ src/main/java/Libros.java
@@ -1,2 +1,11 @@
 public class Libros {
+    private String nombre;
+    private String autor;
+    private String editorial;
+
+    public Libros(String nombre, String autor, String editorial){
+        this.nombre=nombre;
+        this.autor=autor;
+        this.editorial=editorial;
+    }
 }
